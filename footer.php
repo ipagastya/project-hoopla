84b1cb084d4c285681337ed8ef3f5cf9ceffad12
arieft23 <arieftritomo@gmail.com>
2017-07-12 22:24:41 +0700

    delete bootstrap folder, add footer and header

---
+++ footer.php
@@ -0,0 +1,4 @@
+</body>
+<script src="libs/jquery/dist/jquery.min.js"></script>
+<script src="libs/bootstrap//dist/js/bootstrap.min.js"></script>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/dist/css/bootstrap-theme.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand">Hoopla Inventory System</a>
+		</div>
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-left">
+				<li><a href="index">Customer</a></li>
+				<li><a href="menu">Subscription</a></li>
+				<li><a href="contact">Payment</a></li>
+				<li><a href="contact">Inventory</a></li>
+			</ul>
+		</div>
+	</nav>
