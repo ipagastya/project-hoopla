2910d18012e84ef66cea2f72f63a28850ce6462d
arieft23 <arieftritomo@gmail.com>
2017-07-12 23:49:57 +0700

    add login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/dist/css/bootstrap-theme.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class= "container-fluid">
+		<div align= "center">
+			<h3>Hoopla Inventory System</h3>
+			<h3>Login Screen</h3>
+			<form>
+				<input type="text" name="id" placeholder="username"><br>
+				<input type="password" name="password" placeholder="password"><br>
+				<button type="submit">Log In</button>
+			</form>
+		</div>
+	</div>
+</body>
+	<script src="libs/jquery/dist/jquery.min.js"></script>
+	<script src="libs/bootstrap//dist/js/bootstrap.min.js"></script>
+</html>
