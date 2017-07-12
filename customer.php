44d0e3a1088f3583cc4c9ebb73de223f450c278d
arieft23 <arieftritomo@gmail.com>
2017-07-13 00:08:02 +0700

    add customer template no pagination

---
+++ customer.php
@@ -0,0 +1,27 @@
+<?php 
+	require('header.php');
+?>
+<div class= "container-fluid">
+	<div align="right">
+		<button>Add Customer</button>
+	</div>
+	<table>
+		<tr>
+			<td>No</td>
+			<td>Customer's Name</td>
+			<td>Baby's Name</td>
+			<td>Baby's Date of Birth</td>
+			<td>Baby's Age</td>
+			<td>Details</td>
+		</tr>
+		<?php
+			// loop untuk isi db
+		?>
+	</table>
+
+	<!-- belom ada pagination -->
+</div>
+	
+<?php
+	require('footer.php');
+?>
