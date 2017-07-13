15a0f9ac7120edf375c10ec94d7a2a6279115a9f
yosuabisma <yosuabisma@gmail.com>
2017-07-13 22:42:43 +0700

    create welcome.php and change something

---
+++ welcome.php
@@ -0,0 +1,15 @@
+<?php 
+	require('header.php');
+?>
+<div class="welcomepage">
+	<br>
+	<br>
+	<br>
+	<br>
+	<center><img class="img-responsive" src="src/images/logo-1.png"></center>
+	<br>
+	<center><h1>Welcome !</h1></center>
+</div>
+<?php
+	require('footer.php');
+?>
