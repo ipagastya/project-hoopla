3ff485169860d89a44cae4c70df5b133a4a64e80
arieft23 <arieftritomo@gmail.com>
2017-07-13 18:52:40 +0700

    rubah button dikit

--- subscription_list.php
+++ subscription_list.php
@@ -2,30 +2,38 @@
 	require('header.php');
 ?>
 <div class= "container-fluid">
-	<table class="table">
-		<tr>
-			<td></td>
-			<td>Start Date</td>
-			<td>End Date</td>
-		</tr>
-		<tr>
-			<td>Final Pick Up Date</td>
-			<td><input type="date" name="final-date"></td>
-			<td><input type="date" name="end-date"></td>
-		</tr>
-		<tr>
-			<td>Customer Name</td>
-			<td><input type="text" name="name"></td>
-		</tr>
-		<tr>
-			<td>Payment Note</td>
-			<td><input type="text" name="note"></td>
-		</tr>
-		<tr>
-			<td>Subscription Plan</td>
-			<td><input type="number" name="sub-plan"> Month</td>
-		</tr>
-	</table>
+	<form class="form">
+		<table class="table">
+			<tr>
+				<td></td>
+				<td>Start Date</td>
+				<td>End Date</td>
+			</tr>
+			<tr>
+				<td>Final Pick Up Date</td>
+				<td><input type="date" name="final-date"></td>
+				<td><input type="date" name="end-date"></td>
+			</tr>
+			<tr>
+				<td>Customer Name</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Payment Note</td>
+				<td><input type="text" name="note"></td>
+			</tr>
+			<tr>
+				<td>Subscription Plan</td>
+				<td><input type="number" name="sub-plan"> Month</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>
+					<button class="btn btn-primary" id="loginbutton" type="submit"><b>Submit</b></button>
+				</td>
+			</tr>
+		</table>
+	</form>
 	<h5>Subscription Schedule</h5>
 	<table class="table">
 		<tr>
@@ -43,7 +51,7 @@
 		?>
 	</table>
 	<div align="right">
-		<button class="btn" type="button">Add Subscription</button>
+		<button class="btn btn-default addbutton" type="button"><span class="glyphicon glyphicon-plus"></span> Add Subscription</button>
 	</div>
 </div>
 <?php
