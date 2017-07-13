6d1a8a8fa0939df53a5276b386a3f352f15437da
arieft23 <arieftritomo@gmail.com>
2017-07-14 00:31:32 +0700

    add image from agast

--- welcome.php
+++ welcome.php
@@ -9,6 +9,7 @@
 	<center><img class="img-responsive" src="src/images/logo-1.png"></center>
 	<br>
 	<center><h1>Welcome !</h1></center>
+	<img class="img-responsive align-bottom" src="src/images/mainan-smaller.png">
 </div>
 <?php
 	require('footer.php');
