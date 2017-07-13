236f8e1a5ca82293c0e260eacd3ceac9d6e34f5f
yosuabisma <yosuabisma@gmail.com>
2017-07-13 14:02:15 +0700

    improve login screen

--- index.php
+++ index.php
@@ -9,13 +9,27 @@
 <body>
 	<div class= "container-fluid">
 		<div align= "center">
-			<h3>Hoopla Inventory System</h3>
-			<h3>Login Screen</h3>
-			<form class="form">
-				<input type="text" name="id" placeholder="username"><br>
-				<input type="password" name="password" placeholder="password"><br>
-				<button class="btn" type="submit">Log In</button>
-			</form>
+			<br>
+			<br>
+			<center><img class="img-responsive" src="src/images/logo-1.png"></center>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+					</div>
+					<div class="col-sm-4">
+						<h3>Hoopla Inventory System</h3>
+						<h4>Login Screen</h4>
+						<br>
+						<form class="form">
+							<input type="text" class="form-control" name="id" placeholder="username"><br>
+							<input type="password" class="form-control" name="password" placeholder="password"><br>
+							<button class="btn btn-primary" type="submit"><b>LOGIN</b></button>
+						</form>
+					</div>
+					<div class="col-sm-4">
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
