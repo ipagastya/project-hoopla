508632234e8f0eb13627fc58e0ea760e79f0b088
yosuabisma <yosuabisma@gmail.com>
2017-07-14 20:38:05 +0700

    remove br br br

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="src/css/hooplastyle.css">
 	<link rel="icon" type="image/png" href="src/images/mainan-smallest.png">
+	<script src="libs/jquery/dist/jquery.min.js"></script>
+	<script src="libs/bootstrap//dist/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class= "container-fluid index">
@@ -24,20 +26,15 @@
 						<form class="form">
 							<input type="text" class="form-control" name="id" placeholder="username"><br>
 							<input type="password" class="form-control" name="password" placeholder="password"><br>
-							<button class="btn btn-primary" id="loginbutton" type="submit"><b>LOGIN</b></button>
+							<button  class="btn btn-primary" id="loginbutton" type="submit"><b>LOGIN</b></button>
 						</form>
 					</div>
 					<div class="col-sm-4 col-md-4">
 					</div>
 				</div>
 			</div>
-			<br>
-			<br>
-			<br>
-			<br>
 		</div>
 	</div>
 </body>
-	<script src="libs/jquery/dist/jquery.min.js"></script>
-	<script src="libs/bootstrap//dist/js/bootstrap.min.js"></script>
+	
 </html>
