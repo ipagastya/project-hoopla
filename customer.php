652a774d091ad914f523dddbed1432ea454e7b15
ajishoko <yudistira.ramadhan@rocketmail.com>
2017-07-14 22:59:58 +0700

    finalization customer tahap 3

--- customer.php
+++ customer.php
@@ -1,121 +1,111 @@
 <?php 
 	require('header.php');
 ?>
- <div class='container' style='margin-top: 100px;'>
-        <div class='col-sm-3'></div>
-        <div class='col-sm-6'>
-          <form>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="customerID">Customer's Name :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="name" placeholder="Name">
-			  </div>
-			  <div class="col-sm-4"></div>
+ <div class="container">
+		<form class="form-horizontal">
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="customerID">Customer's Name :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="customerID" name="customerID" placeholder="Customer's Name">
+				</div>
+				<div class="col-sm-5"></div>
 			</div>
-              <br>
-			<div id='form-row'>
-             <label class="control-label col-sm-4" for="babyID">Baby's Name :</label>
-			 <div class="col-sm-4">
-              <input type="text" class='form-control' name="babyName" placeholder="Name">
-			 </div>
-			 <div class="col-sm-4"></div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="babyID">Baby's Name :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="babyID" name="babyID" placeholder="Baby's Name">
+				</div>
+				<div class="col-sm-5"></div>
 			</div>
-              <br>
-			<div id='form-row'>
-             <label class="control-label col-sm-4" for="dobID">Baby's Date of Birth :</label>
-			 <div class="col-sm-4">
-              <input type="date" class='form-control' name="date" placeholder="dd/mm/yy">
-			 </div>
-			 <div class="col-sm-4"></div>
-            </div>
-			  <br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="numberID">Home Number / Mobile (WA) :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="number" placeholder="Home Number">
-			  </div>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="number" placeholder="Mobile Number(WA)">
-			  </div>
-              <br>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="dobID">Baby's Date of Birth :</label>
+				<div class="col-sm-5">
+					<input type="date" class="form-control" id="dobID" name="dobID">
+				</div>
+				<div class="col-sm-5"></div>
 			</div>
-            <div id='form-row'>
-              <label class="control-label col-sm-4" for="lineID">Line ID :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="date" placeholder="Line ID">
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			  <br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="lineID">Email :</label>
-			  <div class="col-sm-4">
-				<input type="email" class='form-control' name="date" placeholder="Email">
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			<br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="addressID">Address :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="date" placeholder="Address">
-			  </div>
-            </div>
-			<div class="col-sm-4"></div>
-			<br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="cityID">City :</label>
-			  <div class="col-sm-4">
-				<select class="form-control" id="city" name="city">
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="numberID">Home Number / Mobile Number (WA) :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="homeNumberID" name="homeNumberID" placeholder="Home Number">
+				</div>
+				<div class="col-sm-5">
+				<input type="text" class="form-control" id="mobileNumberID" name="mobileNumberID" placeholder="Mobile Number">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="lineID">Line ID :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="lineID" name="lineID" placeholder="Line ID">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="dobID">Email :</label>
+				<div class="col-sm-5">
+					<input type="email" class="form-control" id="emailID" name="emailID">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="address">Address :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="address" name="address">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="city">City :</label>
+				<div class="col-sm-5">	
+					<select class="form-control" id="city" name="city">
 						<option value=''>Select city</option>
 						<?php
 							// loop isi city dari db
 						?>
 					</select>
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			<br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="provinceID">Province :</label>
-			  <div class="col-sm-4">
-				<select class="form-control" id="province" name="province">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="province">Province :</label>
+				<div class="col-sm-5">	
+					<select class="form-control" id="province" name="province">
 						<option value=''>Select province</option>
 						<?php
 							// loop isi province dari db
 						?>
 					</select>
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			<br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="zipID">ZIP Code :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="date" placeholder="ZIP">
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			<br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="zipID">Favorite / Least Favorite Toys :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="date" placeholder="Favorite">
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			<br>
-			<div id='form-row'>
-              <label class="control-label col-sm-4" for="zipID">Milestones to be developeds :</label>
-			  <div class="col-sm-4">
-				<input type="text" class='form-control' name="date" placeholder="Milestones">
-			  </div>
-			  <div class="col-sm-4"></div>
-            </div>
-			<br>              
-          </form>
-        </div>
-    </div>
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="mobile">ZIP :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="zipID" name="zipID">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="home">Favorite/Least Favorite Toys :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="favoriteID" name="favoriteID">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="deliv-date">Milestones to be developed :</label>
+				<div class="col-sm-5">
+					<input type="text" class="form-control" id="milestoneID" name="milestoneID">
+				</div>
+				<div class="col-sm-5"></div>
+			</div>
+			<div class="form-group">
+			<div class="col-sm-3"></div>
+			<button class="btn btn-primary greenbutton control-label col-sm-1" type="submit"><b class="submit-text">Submit</b></button>
+			<div class="col-sm-8"></div>
+		</div>
+		</form>
+	</div>
 <?php
 	require('footer.php');
 ?>
