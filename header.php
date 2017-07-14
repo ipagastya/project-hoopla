fd9ab3a6fc7589ca5f7593b28b16d4eb3093adba
yosuabisma <yosuabisma@gmail.com>
2017-07-14 23:39:32 +0700

    modify header.php

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a href="welcome.php" class="navbar-brand" style="color:#2ecc71"><b>Hoopla Inventory System</b></a>
+			<a href="welcome.php"><img class="img-responsive" src="src/images/logo-1.png" style="height:50px"></a>
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-left">
