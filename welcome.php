3be6b3dd0d4eb17b07662dd2c6ac3fb4a24a0b70
Agastya <Agastya>
2017-07-14 23:10:17 +0700

    update image

--- welcome.php
+++ welcome.php
@@ -2,6 +2,10 @@
 	require('header.php');
 ?>
 <div class="welcomepage">
+	<br>
+	<br>
+	<br>
+	<br>
 	<center><h1>Welcome To Our Inventory System</h1></center>
 	<img class="img-responsive align-center" src="src/images/mainan-medium.png">
 </div>
