9f4dbf648a66f387d0fd941ce33bfc38b57c6901
Agastya <Agastya>
2017-07-15 01:45:47 +0700

    edit index

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 			<br>
 			<center><img class="img-responsive" src="src/images/logo-1.png"></center>
 			<img class="img-responsive align-center" src="src/images/invent.png">
-			<div class="container">
+			<div>
 				<div class="row">
 					<div class="col-sm-4 col-md-4">
 					</div>
