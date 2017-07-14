712303c3136dc2d1506934226d3939656a5f22b3
Agastya <Agastya>
2017-07-14 23:07:36 +0700

    update  welcome

--- welcome.php
+++ welcome.php
@@ -2,12 +2,6 @@
 	require('header.php');
 ?>
 <div class="welcomepage">
-	<br>
-	<br>
-	<br>
-	<br>
-	<center><img class="img-responsive" src="src/images/logo-1.png"></center>
-	<br>
 	<center><h1>Welcome To Our Inventory System</h1></center>
 	<img class="img-responsive align-center" src="src/images/mainan-medium.png">
 </div>
