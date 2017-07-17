27e42ec62cae045c671070e500bf0a4f5497be0b
arieft23 <arieftritomo@gmail.com>
2017-07-17 22:26:06 +0700

    add </html> footer

--- footer.php
+++ footer.php
@@ -3,3 +3,4 @@
 <script src="libs/bootstrap//dist/js/bootstrap.min.js"></script>
 <script src="libs/bootstrap-select-1.12.3/dist/js/bootstrap-select.min.js"></script>
 <script src="libs/bootstrap-select-1.12.3/dist/js/i18n/defaults-en_US.min.js"></script>
+</html>
