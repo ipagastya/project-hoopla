d6ca66972c77089a732fe4ccf3f92a53b3fe24f4
arieft23 <arieftritomo@gmail.com>
2017-07-18 20:27:25 +0700

    ubah db jadi mysql

--- login_pg.php
+++ login_pg.php
@@ -1,14 +1,14 @@
 <?php
 include "config.php";
 $query = "SELECT username FROM admin";
-$result = mysqli_query($conn, $query);
+$result = pg_query($query);
 if(isset($_POST["loginbutton"])){
   $username = $_POST["id"];
   $password = $_POST["password"];
   $query = "SELECT * FROM admin WHERE username='".$username."'AND password='".$password."'";
-  $result = mysqli_query($query);
-  $row = mysqli_fetch_row($result);
-  $count = mysqli_num_rows($result);
+  $result = pg_query($query);
+  $row = pg_fetch_row($result);
+  $count = pg_num_rows($result);
   if($username == "" && $password == ""){
     echo "<script>alert('Please enter your username and password');</script>";
   }
