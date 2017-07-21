00aced8386b56a5bd49b75bd1c869b7bdef11e2a
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-22 03:34:55 +0700

    Update cdn

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 </body>
 <script src="libs/jquery/dist/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="libs/bootstrap//dist/js/bootstrap.min.js"></script>
 <script src="libs/bootstrap-select-1.12.3/dist/js/bootstrap-select.min.js"></script>
 <script src="libs/bootstrap-select-1.12.3/dist/js/i18n/defaults-en_US.min.js"></script>
