e3ea6bafc107abd7b62a41191f5af71b088d01cd
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-22 02:36:19 +0700

    Added select_city.php

---
+++ select_city.php
@@ -0,0 +1,20 @@
+<?php
+   session_start();
+   include "config.php";
+   require('header.php');
+   
+      if(isset($_POST['province'])){
+         $province = $_POST['province'];
+
+         $sql = "SELECT C.city_id, C.city_name
+               FROM CITY AS C
+               WHERE C.province_id = $periode;"
+
+         $result = mysqli_query($conn , $sql);
+
+      	echo "<option value='default'>Select City</option>";
+	      while($row = mysqli_fetch_row($result)){
+	         "<option value='".$row[0]."' >".$row[1]."</option>";
+	      }
+      }
+?>
