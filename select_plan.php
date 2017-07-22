480c8e4d0fe41b149d68236e61c2026303229db4
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-22 21:03:57 +0700

    Update select_plan.php

--- select_plan.php
+++ select_plan.php
@@ -4,12 +4,13 @@
    
       if(isset($_POST['plan'])/* && isset($_POST['promo')*/){
          $plan = $_POST['plan'];
-	#$promo = (int)$_POST['promo'];
+	 $promo = $_POST['promo'];
          $sql = "SELECT price
                FROM SUBSCRIPTION_TYPE
                WHERE id = $plan";
          $result = mysqli_query($conn , $sql);
 	       $row = mysqli_fetch_row($result);
-	       echo "Rp $row[0]";
+	       //echo "Rp $row[0]";
+	      echo "Rp $promo";
       }
 ?>
