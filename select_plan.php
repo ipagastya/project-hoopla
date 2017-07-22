af1c1f8b0218f231aa79e9b97e18d694c03bd3c2
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-22 18:55:51 +0700

    Updated script

--- select_plan.php
+++ select_plan.php
@@ -2,15 +2,15 @@
    session_start();
    include "config.php";
    
-      if(isset($_POST['plan']) && isset($_POST['promo')){
+      if(isset($_POST['plan']) && /*isset($_POST['promo')*/){
          $plan = $_POST['plan'];
-	 $promo = (int)$_POST['promo'];
+	#$promo = (int)$_POST['promo'];
          $sql = "SELECT price
                FROM SUBSCRIPTION_TYPE
                WHERE id = $plan";
          $result = mysqli_query($conn , $sql);
 	       $row = mysqli_fetch_row($result);
-	       $price = (int)$row[0] - $promo;
+	       $price = $row[0]; #- $promo;
 	       echo "Rp. $price";
       }
 ?>
