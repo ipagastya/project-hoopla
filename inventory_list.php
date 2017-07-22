18648416dbb743e428287d2889d3d7dd6b74629a
yosuabisma <yosuabisma@gmail.com>
2017-07-22 10:26:44 +0700

    edit status form

--- inventory_list.php
+++ inventory_list.php
@@ -15,7 +15,6 @@
 			<label class="control-label col-sm-2" for="inventorystatus">Inventory Status :</label>
 			<div class="col-sm-3">
 				<select class="form-control" id="status" name="status">
-				    <option>--Select Status--</option>
 					<option>Available</option>
 					<option>Rented</option>
 				    <option>Broken</option>
