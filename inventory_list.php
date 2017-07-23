3d52286aed9e09955e91f0ab285cd6d8609c9874
yosuabisma <yosuabisma@gmail.com>
2017-07-24 01:01:23 +0700

    filter function on inventory list

--- inventory_list.php
+++ inventory_list.php
@@ -5,7 +5,8 @@
 	<center><h3>Inventory List</h3></center>
 	<br>
 	<div class="filterbox"><button class="addbutton" data-toggle="collapse" data-target="#form-filter"><span class="glyphicon glyphicon-filter"></span> Filter Subscription Table</button></div>
-	<form class="form-horizontal collapse" id="form-filter">
+	<!--FILTER AREA-->
+	<form class="form-horizontal collapse" id="form-filter" method="post" action="inventory_list.php">
 		<br>
 		<div class="form-group">
 			<label class="control-label col-sm-2" for="dateinventory">Date :</label>
@@ -18,6 +19,7 @@
 			<label class="control-label col-sm-2" for="inventorystatus">Inventory Status :</label>
 			<div class="col-sm-3">
 				<select class="form-control" id="status" name="status">
+					<option>--Select Status Filter--</option>
 					<option>Available</option>
 					<option>Rented</option>
 				    <option>Broken</option>
@@ -29,10 +31,11 @@
 		<br>
 		<div class="form-group">
 			<div class="col-sm-4"></div>
-			<button class="greenbutton control-label col-sm-4" type="submit"><center>Submit</center></button>
+			<button class="greenbutton control-label col-sm-4" type="submit" name="filtersubmit" id="filtersubmit"><center>Submit</center></button>
 			<div class="col-sm-4"></div>
 		</div>
 	</form>
+	<!--============================-->
 	<br>
 	<div align="right">
 		<a href="inventory_create.php"><button class="addbutton" type="button"><span class="glyphicon glyphicon-plus"></span> Add Inventory</button></a>
@@ -56,7 +59,26 @@
 			<tbody>
 			<?php
 				include "config.php";
-				$query = "SELECT * FROM INVENTORY";
+				if(isset($_POST['filtersubmit'])){
+					$date = $_POST['dateinventory'];
+					$status = $_POST['status'];
+					if($date != "" && $status != "--Select Status Filter--"){
+						$query = "SELECT * FROM INVENTORY WHERE return_date = '$date' AND status = '$status'";
+					}
+					if($date != "" && $status == "--Select Status Filter--"){
+						$query = "SELECT * FROM INVENTORY WHERE return_date = '$date'";
+					}
+					if($date == "" && $status != "--Select Status Filter--"){
+						$query = "SELECT * FROM INVENTORY WHERE status = '$status'";
+					}
+					if($date == "" && $status == "--Select Status Filter--"){
+						$query = "SELECT * FROM INVENTORY";
+					}
+
+				}
+				if(!(isset($_POST['filtersubmit']))){
+					$query = "SELECT * FROM INVENTORY";
+				}
 				$result = mysqli_query($conn,$query);
 				if(!$result){
 	               	echo("Couldn't execute query");
