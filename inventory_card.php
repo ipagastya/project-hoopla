fcb89cd92c8985620797fea532c840aa1401d64c
arieft23 <arieftritomo@gmail.com>
2017-07-25 22:37:22 +0700

    gatau bisa yak

--- inventory_card.php
+++ inventory_card.php
@@ -80,11 +80,9 @@
 		$notes = $_POST['notes'];
 		$query="INSERT INTO INVENTORY_CARD(product_code,date,activity_id,Status,note) VALUES('$product_code','$date','$activities','$status','$notes')";
 		$result=mysqli_query($conn,$query);
-		echo"
-		<script>
-			alert('Successfully Updated Inventory Card');
-			window.location.href='inventory_list';
-		</script>";
+		?>
+		<script>alert('Successfully Updated Inventory Card');window.location.href='inventory?id=<?=$inventory_id?>';</script>";
+		<?php
 	}
 ?>
 <?php 
