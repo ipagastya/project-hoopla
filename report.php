3fd7261752c8e8ac6d42037875a84a28542417aa
Adiguna Triputra <adiguna.triputra@gmail.com>
2017-07-26 23:18:24 +0700

    bikin header report.php

--- report.php
+++ report.php
@@ -1,7 +1,22 @@
 <?php
 require('header.php');
 ?>
+<div class= "container">
+    <h4>Report</h4>
+    <div class="table-responsive">
+        <table class="table">
+            <tr>
+                <th>Month/Year</th>
+                <th>New Customer</th>
+                <th>Expired Subscription</th>
+                <th>Extention Subscription</th>
+                <th>Nett Subscription</th>
+                <th>Subscription Commited</th>
+            </tr>
+        </table>
+    </div>
 
+</div>
 <?php
 require('footer.php');
 ?>
