72609580737b2b932f6f4e3ad68d167720c0f4bc
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-26 23:38:22 +0700

    Update report.php

--- report.php
+++ report.php
@@ -6,16 +6,27 @@ require('header.php');
     <div class="table-responsive">
         <table class="table">
             <tr>
-                <th>Month/Year</th>
-                <th>New Customer</th>
-                <th>Expired Subscription</th>
-                <th>Extention Subscription</th>
-                <th>Nett Subscription</th>
-                <th>Subscription Commited</th>
+                <th rowspan="2">Month/Year</th>
+                <th colspan="3">New Customer</th>
+                <th colspan="3">Expired Subscription</th>
+                <th colspan="3">Extending Subscription</th>
+                <th>Nett Subscribers</th>
+                <th rowspan="2">Subscription Commited</th>
+            </tr>
+            <tr>
+                <th>Total</th>
+                <th>1 MO</th>
+                <th>3 MO</th>
+                <th>Total</th>
+                <th>1 MO</th>
+                <th>3 MO</th>
+                <th>Total</th>
+                <th>3 MO</th>
+                <th>6 MO</th>
+                <th>Total</th>
             </tr>
         </table>
     </div>
-
 </div>
 <?php
 require('footer.php');
