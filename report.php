fa081aa8cb21c3ed0f4b3499a9564885d7caaa26
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-27 01:30:42 +0700

    Debugged new customer report

--- report.php
+++ report.php
@@ -61,7 +61,8 @@ include "config.php";
 			    $query3new = "SELECT COUNT(*)
 					FROM SUBSCRIPTION
 					WHERE EXTRACT(MONTH FROM date_added) = '$currentMonth'
-					    AND EXTRACT(YEAR FROM date_added) = '$currentYear' subs_plan = 3
+					    AND EXTRACT(YEAR FROM date_added) = '$currentYear' 
+					    AND subs_plan = 3
 					    AND LOWER(status) = 'new'";
 			
 			    $query6new = "SELECT COUNT(*)
