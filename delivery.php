65bb72ff7bc7bf954565be659eafc86c1396a1c6
Agastya <Agastya>
2017-07-27 11:13:54 +0700

    add favorite toys and milestones form readonly

--- delivery.php
+++ delivery.php
@@ -204,7 +204,7 @@ $baby_age = (date('Y') - date('Y',strtotime($dob)));
 			<div class="col-sm-5"></div>
 		</div>
 		<div class="form-group">
-			<label class="control-label col-sm-2" for="milestones">Favorite Toys :</label>
+			<label class="control-label col-sm-2" for="milestones">Milestones :</label>
 			<div class="col-sm-5">
 				<input type="text" class="form-control" id="milestones" value="<?=$customer['milestones']?>" name="milestones" readonly>
 			</div>
