24ef076e99f3727212d01e63e81fae5a540402ba
arieft23 <arieftritomo@gmail.com>
2017-07-27 19:52:05 +0700

    exec query, not tested yet

--- insertdelivery.php
+++ insertdelivery.php
@@ -16,9 +16,12 @@ $payment_note = $_POST['pay-note'];
 $note = $_POST['note'];
 $baby_age = $_POST['age'];
 $box_name = $_POST['box-name'];
-$sql_deliv = "INSERT INTO DELIVERY_LIST (cust_id, address, city_id, province_id, mobile_phone, home_phone, delivery_date, pickup_date, actual_delivery_charge, actual_pickup_charge, payment_note, note	baby_age, box_name
+$sql_deliv = "INSERT INTO DELIVERY_LIST (cust_id, address, city_id, province_id, mobile_phone, home_phone, delivery_date, pickup_date, actual_delivery_charge, actual_pickup_charge, payment_note, note, baby_age, box_name
 ) VALUES ('$cust_id', '$address', '$city_id', '$province_id', '$mobile_phone', '$home_phone', '$delivery_date', '$pickup_date', '$actual_delivery_charge', '$actual_pickup_charge', '$payment_note', '$note', '$baby_age', '$box_name');";
-echo $sql_deliv."<br>";
+if (($result = mysqli_query($conn, $sql_deliv)) ===  FALSE)) {
+	echo "query to insert delivery fail";
+}
+
 
 $arr = $_POST['select-toy'];
 
@@ -29,8 +32,10 @@ $row = mysqli_fetch_assoc(mysqli_query($conn, $sql));
 $activity_name = $row['activity_name'];
 $activity_id = $row['activity_id'];
 foreach ($arr as &$value) {
-	$sql_card = "INSERT INTO INVENTORY_CARD (date, activity_id, Status, note) VALUES ('$today', '$activity_id', '$activity_name');";
-	echo $sql_card."<br>";
+	$sql_card = "INSERT INTO INVENTORY_CARD (product_code, date, activity_id, Status) VALUES ('$value', '$today', '$activity_id', '$activity_name');";
+	if (($result = mysqli_query($conn, $sql_card)) ===  FALSE)) {
+		echo "query to insert invent_card fail";
+	}
 }
 
 //insert table delivery_toys
@@ -43,18 +48,24 @@ if (mysqli_num_rows(mysqli_query($conn, $sql)) == 0) {
 }
 foreach ($arr as &$value) {
 	$sql_deliv_toy = "INSERT INTO DELIVERY_TOYS (delivery_id, product_code) VALUES ('$id', '$value');";
-	echo $sql_deliv_toy."<br>";
+	if (($result = mysqli_query($conn, $sql_deliv_toy)) ===  FALSE)) {
+		echo "query to insert delivery toy fail";
+	}
 }
 
 //insert table toys_tracking
 foreach ($arr as &$value) {
-	$sql_track = "INSERT INTO TOYS_TRACKING (cust_id, city_id) VALUES ('$cust_id', '$value');";
-	echo $sql_track."<br>";
+	$sql_track = "INSERT INTO TOYS_TRACKING (customer_id, product_code) VALUES ('$cust_id', '$value');";
+	if (($result = mysqli_query($conn, $sql_track)) ===  FALSE)) {
+		echo "query to insert tracking fail";
+	}
 }
 
 // update table inventory
 foreach ($arr as &$value) {
 	$sql_inventory = "UPDATE INVENTORY SET status='Rented' WHERE product_code='$value';";
-	echo $sql_inventory."<br>";
+	if (($result = mysqli_query($conn, $sql_inventory)) ===  FALSE)) {
+		echo "query to update inventory fail";
+	}
 }
 ?>
