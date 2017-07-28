7f604cf806d88b47a7af37224c4d2aa473e9c52b
arieft23 <arieftritomo@gmail.com>
2017-07-29 00:04:17 +0700

    aktifin filter

--- subscription_list.php
+++ subscription_list.php
@@ -3,7 +3,7 @@
 ?>
 <div class= "container">
 	<button class="addbutton" data-toggle="collapse" data-target="#form-filter"><span class="glyphicon glyphicon-filter"></span> filter</button>
-	<form class="form-horizontal collapse" id="form-filter">
+	<form class="form-horizontal collapse" id="form-filter" method="post" action="subscription_list">
 		<div class="form-group">
 			<label class="control-label col-sm-3" for="start-date">Final Pickup Date :</label>
 		</div>
@@ -34,14 +34,14 @@
 		<div class="form-group">
 			<label class="control-label col-sm-3" for="sub-plan">Subscription Plan :</label>
 			<div class="col-sm-3">
-				<input type="text" class="form-control" id="sub-plan" name="sub-plan">
+				<input type="number" class="form-control" id="sub-plan" name="sub-plan">
 			</div>
 			<label class="control-label col-sm-1" for="sub-plan">Month</label>
 			<div class="col-sm-5"></div>
 		</div>
 		<div class="form-group">
 			<div class="col-sm-3"></div>
-			<button class="greenbutton control-label col-sm-1" type="submit"><b class="submit-text">Submit</b></button>
+			<button class="greenbutton control-label col-sm-1" type="submit" name="filtersubmit" id="filtersubmit"><center>Submit</center></button>
 			<div class="col-sm-8"></div>
 		</div>
 	</form>
@@ -65,6 +65,51 @@
 			<?php
 				include "config.php";
 				$sql = "SELECT subs_id, cust_name, status, subs_plan, num_ofToys, first_deliv, final_pickup, payment_terms FROM SUBSCRIPTION AS S JOIN CUSTOMER AS C ON S.cust_id = C.cust_id ";
+				if(isset($_POST['filtersubmit'])){
+					$ada = false;
+					$condition = "";
+					$condition_date = "";
+					$condition_customer = "";
+					$condition_payment = "";
+					$condition_plan = "";
+					if(isset($_POST['start-date']) && isset($_POST['end-date']) && $_POST['start-date'] && $_POST['end-date']){
+						$start_date = $_POST['start-date'];
+						$end_date = $_POST['end-date'];
+						$condition_date = "(final_pickup BETWEEN '$start_date' AND '$end_date')";
+						$ada = true;
+					}
+					if (isset($_POST['name']) && $_POST['name']) {
+						if ($ada) {
+							$condition_customer = $condition_customer." AND ";
+						}
+						$name = $_POST['name'];
+						$condition_customer = "(cust_name LIKE '%$name%')";
+						$ada = TRUE;
+					}
+					if (isset($_POST['note']) && $_POST['note']) {
+						if ($ada) {
+							$condition_payment = $condition_payment." AND ";
+						}
+						$payment = $_POST['note'];
+						$condition_payment = "(payment_terms LIKE '%$payment%')";
+						$ada = TRUE;
+					}
+
+					if (isset($_POST['sub-plan']) && $_POST['sub-plan']) {
+						if ($ada) {
+							$condition_plan = $condition_plan." AND ";
+						}
+						$plan = $_POST['sub-plan'];
+						$condition_plan = "(subs_plan = '$plan')";
+						$ada = TRUE;
+					}
+
+					if($ada){
+						$condition = $condition." WHERE ".$condition_date.$condition_customer.$condition_payment.$condition_plan;
+						$sql = $sql.$condition;
+					}
+					
+				}
 				if (($result = mysqli_query($conn, $sql)) === FALSE){
 					echo "query failing";
 				}
