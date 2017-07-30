a5a8e838ea6eba332ab14146ca5be3995c264832
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-31 00:24:56 +0700

    mysqli edit

--- report_download.php
+++ report_download.php
@@ -1,26 +1,31 @@
 <?php
 	require('header.php');
-  include "config.php";
+	include "config.php";
   
-  $result = $db_con->query('SELECT * FROM INVENTORY_AVAILABLE');
-  if (!$result) die('Couldn\'t fetch records');
-  $num_fields = mysql_num_fields($result);
-  $headers = array();
-  for ($i = 0; $i < $num_fields; $i++) {
-      $headers[] = mysql_field_name($result , $i);
-  }
-  $fp = fopen('php://output', 'w');
-  if ($fp && $result) {
-      header('Content-Type: text/csv');
-      header('Content-Disposition: attachment; filename="report.csv"');
-      header('Pragma: no-cache');
-      header('Expires: 0');
-      fputcsv($fp, $headers);
-      while ($row = $result->fetch_array(MYSQLI_NUM)) {
-          fputcsv($fp, array_values($row));
-      }
-      die;
-  }
-  
-  require('footer.php');
+	$query = 'SELECT * FROM INVENTORY_AVAILABLE';
+	$result = mysqli_query($conn, $query);                          
+	if(!$result) {
+		print("Couldn't execute query");
+		die(mysqli_connect_error());
+	}
+ 
+	 $num_fields = mysqli_num_fields($result);
+	 $headers = array();
+	 for ($i = 0; $i < $num_fields; $i++) {
+	     $headers[] = mysqli_field_name($result , $i);
+	 }
+	 $fp = fopen('php://output', 'w');
+	 if ($fp && $result) {
+	     header('Content-Type: text/csv');
+	     header('Content-Disposition: attachment; filename="report.csv"');
+	     header('Pragma: no-cache');
+	     header('Expires: 0');
+	     fputcsv($fp, $headers);
+	     while ($row = mysqli_fetch_array($result)) {
+		fputcsv($fp, array_values($row));
+	     }
+	     die;
+	 }
+
+	 require('footer.php');
 ?>
