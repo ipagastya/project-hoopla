d55b42b7a932d407767377393930eea815a86f6c
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-31 00:45:10 +0700

    Update report_download.php

--- report_download.php
+++ report_download.php
@@ -1,16 +1,24 @@
 <?php
-	require('header.php');
-	include "config.php";
-  
-	$result = mysqli_query($conn, 'SELECT * FROM INVENTORY_AVAILABLE');
-	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-	$fp = fopen('report.csv', 'w');
-
-	foreach ($row as $val) {
-	    fputcsv($fp, $val);
+	$query = "SELECT * FROM CUSTOMER";
+	$result = $db->query($query);
+	if (!$result) die('Couldn\'t fetch records');
+	$headers = $result->fetch_fields();
+	foreach($headers as $header) {
+	    $head[] = $header->name;
 	}
-	fclose($fp);
+	$fp = fopen('php://output', 'w');
+
+	if ($fp && $result) {
+	    header('Content-Type: text/csv');
+	    header('Content-Disposition: attachment; filename="export.csv"');
+	    header('Pragma: no-cache');
+	    header('Expires: 0');
+	    fputcsv($fp, array_values($head)); 
+	    while ($row = $result->fetch_array(MYSQLI_NUM)) {
+		fputcsv($fp, array_values($row));
+	    }
+	    die;
+	}
 ?>
 
 <?php
