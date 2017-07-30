4df7c91ae6706dcc9d4d147378ab00f790af1efe
Andre Gema Syahputra <andregputra@gmail.com>
2017-07-31 01:15:41 +0700

    Output debugging 2.0

--- report_download.php
+++ report_download.php
@@ -20,7 +20,7 @@
 	     header('Pragma: no-cache');
 	     header('Expires: 0');
 	     fputcsv($fp, $headers);
-	     while ($row = mysqli_fetch_array($result)) {
+	     while ($row = $result->fetch_array(MYSQLI_NUM)) {
 		fputcsv($fp, array_values($row));
 	     }
 	     die;
