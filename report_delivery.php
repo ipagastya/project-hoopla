befa97e9e2a93df67148f096809e8cc1ebc39db4
andregema <andregputra@gmail.com>
2017-08-03 01:19:53 +0700

    Implode test delivery report

--- report_delivery.php
+++ report_delivery.php
@@ -77,13 +77,14 @@
 							<td>";
 						
 						//$flag = TRUE;
-						while($rowSub = $resultSub->fetch_array(MYSQLI_NUM)) {
-								/*	if($flag) {
-										echo $rowSub[0];
-										$flag = FALSE;
-									} else {
-										echo ", ".$rowSub[0]."";
-									}*/
+						while($rowSub = mysqli_fetch_row($resultSub)) {
+							/*if($flag) {
+								echo $rowSub[0];
+								$flag = FALSE;
+							} else {
+								echo ", ".$rowSub[0]."";
+							}*/
+
 							echo implode(", ", $rowSub);
 						}
 						echo "</td><td>$date</td></tr>";
