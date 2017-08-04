4ec069228fe3b8fe63a02f2b3ab626dbfba90b37
Agastya <Agastya>
2017-08-05 02:59:58 +0700

    add tombol

---
+++ libs/return.php
@@ -0,0 +1,20 @@
+<?php 
+if (!isset($_GET['deliv_id'])) {
+	header('Location: susbscription_list');
+}else{
+	require_once('config.php');
+	$deliv_id = $_GET['deliv_id'];
+	$subs_id = $_GET['subs_id'];
+	$sql = "SELECT product_code FROM DELIVERY_TOYS WHERE delivery_id = '$deliv_id'";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+
+	foreach ($row as &$value) {
+		$sql_inventory = "UPDATE INVENTORY SET status='Available' WHERE product_code='$value';";
+		if (($result = mysqli_query($conn, $sql_inventory)) ===  FALSE) {
+			echo "query to update inventory fail";
+		}
+	}
+	header( "refresh:1;url=../subscription?page=1&subs_id=$subs_id" );
+}
+?>
