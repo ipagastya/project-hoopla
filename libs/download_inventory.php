bfc2e0d2b71616f6bb8289f5070a6f24b55801f6
Agastya <Agastya>
2017-08-04 18:39:30 +0700

    test

--- libs/download_inventory.php
+++ libs/download_inventory.php
@@ -16,8 +16,8 @@
 
 	$fp = fopen('php://output', 'w');
 	if ($fp && $result) {
-	    header('Content-Type: text/csv');
-	    header('Content-Disposition: attachment; filename='.$name.'.csv');
+	    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+	    header('Content-Disposition: attachment; filename='.$name.'.xlsx');
 	    header('Pragma: no-cache');
 	    header('Expires: 0');
 	    fputcsv($fp, $type);
