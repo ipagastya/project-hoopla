7d51694edc9f7c9133e094b85fefedc30b670733
Agastya <Agastya>
2017-08-04 14:00:55 +0700

    add table  mobilephone/home

--- report_delivery.php
+++ report_delivery.php
@@ -37,13 +37,13 @@
 			<thead>
 				<tr>
 					<th colspan="4">Customer</th>
-					<th rowspan="1">Toys</th>
-					<th rowspan="1">Delivery Date</th>
+					<th rowspan="2">Toys</th>
+					<th rowspan="2">Delivery Date</th>
 				</tr>
 				<tr>
 					<th>Name</th>
 					<th>Address</th>
-					<th>Home</th>>
+					<th>Home</th>
 					<th>Mobile</th>
 				</tr>
 			</thead>
