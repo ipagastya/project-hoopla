0888d54179b6b05138e9b8cd26739c74ca08e321
arieft23 <arieftritomo@gmail.com>
2017-08-04 16:45:09 +0700

    if city not in shipping_cost list, deliv_price 0

--- libs/deliv_price.php
+++ libs/deliv_price.php
@@ -3,24 +3,27 @@ session_start();
 include "../config.php";
 
 if(isset($_POST['customer']) && isset($_POST['promo'])){
-   $customer = $_POST['customer'];
-   $promo = $_POST['promo'];
+	$customer = $_POST['customer'];
+	$promo = $_POST['promo'];
 
-   $sql = "SELECT city_id
-   FROM CUSTOMER
-   WHERE cust_id = '$customer'";
+	$sql = "SELECT city_id
+	FROM CUSTOMER
+	WHERE cust_id = '$customer'";
 
-   $result = mysqli_query($conn ,$sql);
-   $row = mysqli_fetch_row($result);
-   $city_id = $row[0];
+	$result = mysqli_query($conn ,$sql);
+	$row = mysqli_fetch_row($result);
+	$city_id = $row[0];
 
-   $sql = "SELECT price
-   FROM SHIPPING_COST
-   WHERE regional_id = '$city_id';";
+	$sql = "SELECT price
+	FROM SHIPPING_COST
+	WHERE regional_id = '$city_id';";
 
-   $result = mysqli_query($conn ,$sql);
-   $row = mysqli_fetch_row($result);
-   $price = $row[0] - $promo;
-   echo number_format($price, 2);
+	$result = mysqli_query($conn ,$sql);
+	$row = mysqli_fetch_row($result);
+	$price = $row[0] - $promo;
+	if ($price < 0) {
+		$price = 0;
+	}
+	echo number_format($price, 2);
 }
 ?>
