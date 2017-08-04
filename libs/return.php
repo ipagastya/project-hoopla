fc14bea264f04c74a0f16df78841bddb1f20e649
Agastya <Agastya>
2017-08-05 04:21:53 +0700

    add tombol

--- libs/return.php
+++ libs/return.php
@@ -7,10 +7,10 @@ if (!isset($_GET['deliv_id'])) {
 	$subs_id = $_GET['subs_id'];
 	$sql = "SELECT * FROM DELIVERY_TOYS WHERE delivery_id = '$deliv_id'";
 	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_assoc($result);
-	$prod_code = $row['product_code'];
+	
 
-	while($row){
+	while($row = mysqli_fetch_assoc($result)){
+	    $prod_code = $row['product_code'];
 		$sql_inventory = "UPDATE INVENTORY SET status='Available' WHERE product_code='$prod_code'";
 		if (($res = mysqli_query($conn, $sql_inventory)) ===  FALSE) {
 			echo "query to update inventory fail";
