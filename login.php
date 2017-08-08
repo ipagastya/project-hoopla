ff92925a3482604e077e7a9f79ca9d8f3841cc35
yosuabisma <yosuabisma@gmail.com>
2017-08-09 01:14:05 +0700

    login aneh

--- login.php
+++ login.php
@@ -1,6 +1,42 @@
 <?php
 include "config.php";
-$query = "SELECT username FROM admin";
+if(isset($_POST['loginbutton'])){
+    $username = $_POST['id'];
+    $password = md5($_POST['password']);
+    $user_id = 0;
+    $status = "";
+
+    $stmt = $conn->prepare("SELECT user_id, username, password, status FROM users WHERE username=? AND password=? LIMIT 1");
+    $stmt->bind_param('ss', $username, $password);
+    $stmt->execute();
+    $stmt->bind_result($user_id, $username, $password, $status);
+    $stmt->store_result();
+    if($stmt->num_rows == 1)  //To check if the row exists
+        {
+            if($stmt->fetch()) //fetching the contents of the row
+            {
+               if ($status == 'd') {
+                   echo "YOUR account has been DEACTIVATED.";
+                   exit();
+               } else {
+                   $_SESSION['username'] = $username;
+                   echo 'Success!';
+                   exit();
+               }
+           }
+
+    }
+    else {
+        echo "INVALID USERNAME/PASSWORD Combination!";
+    }
+    $stmt->close();
+}
+else 
+{   
+
+}
+$conn->close();
+/*$query = "SELECT username FROM admin";
 $result = mysqli_query($conn, $query);
 if(isset($_POST["loginbutton"])){
   $username = $_POST["id"];
@@ -22,5 +58,5 @@ if(isset($_POST["loginbutton"])){
       echo"<script>alert('Login Failed !');</script>";
     }
   }
-}
+}*/
 ?>
