5f44bc0fd592b4174c79dd88818734ae6e10d2ac
yosuabisma <yosuabisma@gmail.com>
2017-08-17 00:25:09 +0700

    add upload instruction card

---
+++ libs/upload_instruction_card.php
@@ -0,0 +1,12 @@
+<?php
+	session_start();
+	$canUpload = false;
+	if(isset($_FILES['upload'])){ 
+	    $file_name = $_FILES['upload']['name'];
+	    $file_tmp = $_FILES['upload']['tmp_name'];
+	}
+	move_uploaded_file($file_tmp, ("instruction-card/".$file_name));
+	echo "	<script>alert('Upload Instruction Card Success');
+					document.location.href='../inventory_list?page=1';
+			</script>";
+?>
