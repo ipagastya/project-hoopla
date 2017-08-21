cf7c4704267e2b734cf5f53a19531f5cf4c36a35
arieft23 <arieftritomo@gmail.com>
2017-08-21 22:00:38 +0700

    fix header

--- libs/delete_delivery.php
+++ libs/delete_delivery.php
@@ -2,21 +2,17 @@
 require_once("../config.php");
 if (isset($_GET['deliv_id'])) {
 	$deliv_id = $_GET['deliv_id'];
-
+	
 	// Getting data delivery toys
 	$sql_getToys = "SELECT * FROM DELIVERY_TOYS WHERE delivery_id = '$deliv_id'";
-	echo $sql_getToys."<br>";
 	$result_getToys = mysqli_query($conn, $sql_getToys);
 	if ($result_getToys === FALSE) {
 		echo "Getting data from delivery toys error<br>";
-	}else{
-		echo "Getting data from delivery toys success<br>";
 	}
 
 	// Insert inventory card
 	$today = date("Y-m-d");
 	$sql = "SELECT * FROM INVENTORY_ACTIVITY WHERE activity_name = 'Returned';";
-	echo $sql."<br>";
 	$row = mysqli_fetch_assoc(mysqli_query($conn, $sql));
 	$activity_name = $row['activity_name'];
 	$activity_id = $row['activity_id'];
@@ -35,25 +31,19 @@ if (isset($_GET['deliv_id'])) {
 				echo "Update Inventory Error<br>";
 			}
 		}
-		else{
-			echo "kok gitu";
-		}
+		
 	}
 
 	// Delete from delivery_list table
 	$sql_deliv = "DELETE FROM DELIVERY_LIST WHERE delivery_id = '$deliv_id'";
 	if (mysqli_query($conn, $sql_deliv) === FALSE) {
 		echo "Deleting from delivery list error<br>";
-	}else{
-		echo "Deleting from delivery list Success<br>";
 	}
 
 	// Delete from toys tracking
 	$sql_track = "DELETE FROM TOYS_TRACKING WHERE delivery_id = '$deliv_id'";
 	if (mysqli_query($conn, $sql_track) === FALSE) {
 		echo "Deleting from toys tracking error<br>";
-	}else{
-		echo "Deleting from toys tracking success<br>";
 	}
 
 	
@@ -63,9 +53,10 @@ if (isset($_GET['deliv_id'])) {
 		echo "Delete from delivery toys error<br>";
 	}
 	$subs_id = $_GET['subs_id'];
-	header( "Location=../subscription?page=1&subs_id=$subs_id" );
+	echo "<h1>Please wait, Deleting.....</h1>";
+	header( "refresh:1;url=../subscription?page=1&subs_id=$subs_id" );
 }else{
-	header( "Location=../subscription_list?page=1" );
+	header( "refresh:1;url=../subscription_list?page=1" );
 }
 
 
